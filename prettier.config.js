04830750e6ef6be1c9f53c9eee15f5de082610c9
MorisatoK <accounts@badlands-of-my-mind.de>
2021-08-12 10:17:42 +0200

    Setting up basic stuff

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: false,
+    jsxBracketSameLine: false,
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    useTabs: false,
+};
